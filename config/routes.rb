2b927895d937416660066089c6c8864280e3327f
EC2 Default User <yotchan32323@gmail.com>
2023-01-07 23:38:04 +0000

    [Add] 更新機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get "lists/:id" => "lists#show", as: "list"
   get "lists/:id/edit" => "lists#edit", as: "edit_list"
   get "top" => "homes#top"
+  patch "lists/:id" => "lists#update", as: "update_list"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
